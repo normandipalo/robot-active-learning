266d2eae8c210e98701abed13bfecce1a84d490a
Norman D <normandipalo@gmail.com>
2019-06-06 12:35:48 +0200

    updated count_errors to include full traj prediction

--- count_errors.py
+++ count_errors.py
@@ -19,7 +19,7 @@ hyperp = {"INITIAL_TRAIN_EPS" : 30,
 "BC_BS" : 64,
 "BC_EPS" : 400,
 
-"AE_HD" : 256,
+"AE_HD" : 8,
 "AE_HL" : 1,
 "AE_LR" : 1e-2,
 "AE_BS" : 64,
@@ -30,7 +30,7 @@ hyperp = {"INITIAL_TRAIN_EPS" : 30,
 "ACTIVE_ERROR_THR" : 1.5,
 
 "ORG_TRAIN_SPLIT" : 1.,
-"FULL_TRAJ_ERROR" : False,
+"FULL_TRAJ_ERROR" : True,
 "CTRL_NORM" : True,
 "RENDER_TEST" : False}
 
@@ -65,7 +65,7 @@ def get_experience(eps, env):
         new_states, new_acts = man_controller.get_demo(env, state, CTRL_NORM)
         states+=new_states
         actions+=new_acts
-                
+
     return states, actions
 
 def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = False):
@@ -78,7 +78,7 @@ def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = F
         env = utils.set_state(env, test_set[i][0], test_set[i][1])
         state, *_ = env.step([0.,0.,0.,0.])
         picked = [False]
-        
+
         error = ae.error((np.concatenate((state["observation"],
                                     state["achieved_goal"],
                                     state["desired_goal"])).reshape((1,-1)) - xm)/xs)
@@ -93,7 +93,7 @@ def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = F
             new_state, *_ = env.step(action[0])
          #   print(action)
             if render: env.render()
-            
+
             state = new_state
             if fulltraj:
             #If I choose to sum up the errors of the entire trajectory I integrate errors
@@ -107,7 +107,7 @@ def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = F
           #      print("SUCCESS!")
                 succeded = 1
                 successes +=1
-                
+
                 #divide by number of steps to get an average
                 if fulltraj:
                     error_succ += tot_error/(i + 1)
@@ -116,7 +116,7 @@ def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = F
                     error_succ += tot_error
                     succ_errors_list.append(tot_error)
                 break
-        if not succeded: 
+        if not succeded:
          #   print("FAILURE")
             failures+=1
             #divide by number of steps to get an average
@@ -126,14 +126,14 @@ def test(model, ae, test_set, env, xm, xs, am, ast, fulltraj = False, render = F
             else:
                 error_fail += tot_error
                 fail_errors_list.append(tot_error)
-                
+
     return successes, failures, error_succ/successes, error_fail/failures, succ_errors_list, fail_errors_list
-    
+
 def predict(model, ae, test_set, env, xm, xs, am, ast, tot_error_trainset):
     succ_tp, succ_fp, succ_tn, succ_fn = 0,0,0,0
     successes, failures = 0, 0
     for i in range(len(test_set)):
-        
+
         env.reset()
         env = utils.set_state(env, test_set[i][0], test_set[i][1])
         state, *_ = env.step([0.,0.,0.,0.])
@@ -142,13 +142,17 @@ def predict(model, ae, test_set, env, xm, xs, am, ast, tot_error_trainset):
         error = ae.error((np.concatenate((state["observation"],
                                     state["achieved_goal"],
                                     state["desired_goal"])).reshape((1,-1)) - xm)/xs)
-                            
-        if error > 2.3*tot_error_trainset:
-            prediction = "failure"
-        else:
-            prediction = "success"
+
+        # If not FULL_TRAJ predict directly the outcome. Otherwise, predict success and eventually
+        # correct to failure if the error becomes too high.
+        if not FULL_TRAJ:
+            if error > tot_error_trainset:
+                prediction = "failure"
+            else:
+                prediction = "success"
+        else: prediction = "success"
       #  prediction = "success" #assume you think you'll always succeed
-            
+
         for i in range(100):
             action = model((np.concatenate((state["observation"],
                                         state["achieved_goal"],
@@ -156,10 +160,18 @@ def predict(model, ae, test_set, env, xm, xs, am, ast, tot_error_trainset):
 
             action = action*ast + am
             new_state, *_ = env.step(action[0])
+            if FULL_TRAJ:
+                # Checks at every step if the error becomes too high.
+                error = ae.error((np.concatenate((state["observation"],
+                                            state["achieved_goal"],
+                                            state["desired_goal"])).reshape((1,-1)) - xm)/xs)
+                if error > 1.5*tot_error_trainset:
+                    #print("Error became too high.")
+                    prediction = "failure"
          #   print(action)
-            
+
             state = new_state
-       
+
 
             if not np.linalg.norm((state["achieved_goal"]- state["desired_goal"])) > 0.07:
           #      print("SUCCESS!")
@@ -170,23 +182,23 @@ def predict(model, ae, test_set, env, xm, xs, am, ast, tot_error_trainset):
                 elif prediction == "failure":
                     succ_fn +=1
                 break
-                
+
                 #divide by number of steps to get an average
 
-        if not succeded: 
+        if not succeded:
             failures += 1
             if prediction == "failure":
                 succ_tn +=1
             elif prediction == "success":
                 succ_fp += 1
-                
+
     print("successes, failures", successes, failures)
     return succ_tp, succ_fp, succ_tn, succ_fn
-        
-    
+
+
 
 def get_active_exp(env, threshold, ae, xm, xs, render):
-    
+
     err_avg = 0
     for i in range(20):
         state = env.reset()
@@ -195,7 +207,7 @@ def get_active_exp(env, threshold, ae, xm, xs, render):
                                     state["desired_goal"])).reshape((1,-1)) - xm)/xs)
         err_avg+=error
     err_avg/=20
-    
+
     state = env.reset()
     error = ae.error((np.concatenate((state["observation"],
                                     state["achieved_goal"],
@@ -219,11 +231,11 @@ def get_active_exp(env, threshold, ae, xm, xs, render):
 def go(seed):
     if not tf.__version__ == "2.0.0-alpha0":
         tf.random.set_random_seed(seed)
-    else: 
+    else:
         tf.random.set_seed(seed)
     env = gym.make("FetchPickAndPlace-v1")
     env.seed(seed)
-    
+
     test_set = []
     for i in range(TEST_EPS):
         state = env.reset()
@@ -239,7 +251,7 @@ def go(seed):
     xm = x.mean()
     xs = x.std()
     x = (x - x.mean())/x.std()
-    
+
     a = np.array(actions)
     am = a.mean()
     ast = a.std()
@@ -251,45 +263,46 @@ def go(seed):
     ae = RandomNetwork(1, AE_HD, AE_HL, AE_LR)
 
     ae.train(x, AE_BS, AE_EPS)
-    
+
     tot_error_trainset = 0
     for el in x:
         error = ae.error(el.reshape((1,-1)))
         tot_error_trainset+=error
     tot_error_trainset/=len(x)
-    
+
     print("Average error on train set", tot_error_trainset)
     file.write(str("Average error on train set") +str(tot_error_trainset))
-    
+
     # I try to estimate the error on full train trajectories by multiplying
     # the average by len(dataset)/num_episodes, that is basically
     # the average episode lenght.
-    
+
     if FULL_TRAJ:
         tot_error_train_fulltraj = 0
         avg_ep_lenght = len(x)/INITIAL_TRAIN_EPS
         tot_error_train_fulltraj = tot_error_trainset*avg_ep_lenght
-    
+    #    tot_error_trainset = tot_error_train_fulltraj
+
         print("Average full trajectory error on train set", tot_error_train_fulltraj)
         file.write(str("Average full trajectory error on train set") + str(tot_error_train_fulltraj))
     succ, fail, error_avg_s, error_avg_f, succ_list, fail_list = test(net, ae, test_set, env, xm, xs, am, ast, fulltraj = FULL_TRAJ, render = RENDER_TEST)
     print("Active learning results ", seed, " : ", succ, fail, "avg error on succ trails: ", error_avg_s, "on fail: ", error_avg_f, "std on succ:", np.std(succ_list), "on fail:", np.std(fail_list))
     file.write(str("Active learning results ") + str(seed) +  str(" : ") + str(succ) + str(fail) + str(error_avg_s) + str(error_avg_f) + str(np.std(succ_list)) +  str(np.std(fail_list)))
-  
+
   #  file.write(str("Active learning results " + str(seed) + " : " + str(result_t)))
-    
-    
-    
+
+
+
     succ_tp, succ_fp, succ_tn, succ_fn = predict(net, ae, test_set, env, xm, xs, am, ast, tot_error_trainset)
-    
+
     #change to consider failures
-    #succ_tp, succ_fp, succ_tn, succ_fn = succ_tn, succ_fn, succ_tp, succ_fp
-    
+    succ_tp, succ_fp, succ_tn, succ_fn = succ_tn, succ_fn, succ_tp, succ_fp
+
     print("succ tp, fp, tn, fn", succ_tp, succ_fp, succ_tn, succ_fn)
     precision = (succ_tp/(succ_tp+succ_fp + 0.001))
     recall = (succ_tp/(succ_tp+succ_fn))
-    print("F1 score", (precision*recall/(precision + recall)))
-        
+    print("F1 score", (2*precision*recall/(precision + recall)))
+
 
 
 if __name__ == "__main__":
@@ -303,5 +316,3 @@ if __name__ == "__main__":
             file.write(str(hyperp))
             file.write("\n\n")
             go(k)
-
-    
